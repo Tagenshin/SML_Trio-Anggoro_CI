5f9f3a81877d96128a5f4b5c087aca404079572c
tagenshin <trioanggoro12345@gmail.com>
2025-11-12 20:50:15 +0700

    up

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     # Contoh input untuk logging
     input_example = X_train.head(5)
 
-    # # === MLflow Logging ===
+    # === MLflow Logging ===
     # mlflow.set_tracking_uri("http://127.0.0.1:5000")
     # mlflow.set_experiment("SML_Trio-Anggoro_CI")
 
